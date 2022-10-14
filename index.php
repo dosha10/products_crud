0735f34e971026d0c4cafc73c9e614ed4e59f4ae
dosha10 <ndungufrank01@gmail.com>
2022-10-14 14:32:43 +0300

    php logic

--- index.php
+++ index.php
@@ -7,9 +7,6 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $statement = $pdo->prepare("SELECT * FROM products ORDER BY create_date DESC");
 $statement->execute();
 $products = $statement->fetchAll(PDO::FETCH_ASSOC);
-echo '<pre>';
-var_dump($products);
-echo '<pre>';
 
 ?>
 
@@ -31,34 +28,36 @@ echo '<pre>';
   </head>
   <body>
     <h1>Products CRUD</h1>
+
+    <p>
+      <a href="create.php" class="btn btn-success">Create Product</a>
+    </p>
+
     <table class="table">
       <thead>
         <tr>
           <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
+          <th scope="col">Image</th>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Create Date</th>
+          <th scope="col">Action</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-          <td>@twitter</td>
-        </tr>
+        <?php foreach ($products as $i => $product): ?>
+          <tr>
+            <th scope="row"><?php echo $i +1 ?></th>
+            <td></td>
+            <td><?php echo $product['title'] ?></td>
+            <td><?php echo $product['price'] ?></td>
+            <td><?php echo $product['create_date'] ?></td>
+            <td>
+              <button type="button" class="btn btn-sm btn-outline-primary">Edit</button>
+              <button type="button" class="btn btn-sm btn-outline-danger">Delete</button>
+            </td>
+          </tr>
+         <?php endforeach; ?>
       </tbody>
     </table>
   </body>
