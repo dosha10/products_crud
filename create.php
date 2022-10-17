19d3a50f0bb11e65ecb2dcd49425bc4b27294e9c
dosha10 <ndungufrank01@gmail.com>
2022-10-17 12:57:40 +0300

    fixed form validation issue

--- create.php
+++ create.php
@@ -10,22 +10,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $price = $_POST['price'];
     $date = date('Y-m-d H:i:s');
 
-    if(!$title) {
-      $errors[] = 'Product title is required';
+    if (!$title) {
+        $errors[] = 'Product title is required';
     }
 
-    if(!$price) {
-      $errors[] = 'Product price is required';
+    if (!$price) {
+        $errors[] = 'Product price is required';
     }
- 
-    $statement = $pdo->prepare("INSERT INTO products (title, image, description, price, create_date)
+
+    if (empty($errors)) {
+
+        $statement = $pdo->prepare("INSERT INTO products (title, image, description, price, create_date)
                                   VALUES (:title, :image, :description, :price, :date)");
-    $statement->bindValue(':title', $title);
-    $statement->bindValue(':image', '');
-    $statement->bindValue(':description', $description);
-    $statement->bindValue(':price', $price);
-    $statement->bindValue(':date', $date);
-    $statement->execute();
+        $statement->bindValue(':title', $title);
+        $statement->bindValue(':image', '');
+        $statement->bindValue(':description', $description);
+        $statement->bindValue(':price', $price);
+        $statement->bindValue(':date', $date);
+        $statement->execute();
+    }
 }
 
 ?>
