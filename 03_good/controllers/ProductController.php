0fff1e188e2079621d77f23c38dddb784d6d8a0a
dosha10 <ndungufrank01@gmail.com>
2022-10-25 11:07:48 +0300

    MVC Setup & Configuration

---
+++ 03_good/controllers/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\controllers;
+use app\Router;
+
+class ProductController {
+    public function index() {
+        echo "Index Page";
+    }
+
+    public function create() {
+        echo "Create Page";
+    }
+
+    public function update() {
+        echo "Update page";
+    }
+
+    public function delete() {
+        echo "Delete page";
+    }
+}
---
+++ 03_good/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'frankndungu/03_good',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd13057a7275054b7fcd5ad1e3e10caf0d089f250',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'frankndungu/03_good' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd13057a7275054b7fcd5ad1e3e10caf0d089f250',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
